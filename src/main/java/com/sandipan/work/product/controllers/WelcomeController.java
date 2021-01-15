6d45bdff377b4fd2cfb71b301b8c5372598c82e1
sandy007sandipan <$Passw0rd$>
2021-01-15 13:37:06 +0530

    Coverity Scanning

--- src/main/java/com/sandipan/work/product/controllers/WelcomeController.java
+++ src/main/java/com/sandipan/work/product/controllers/WelcomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product")
-@CrossOrigin(origins = "http://aws-dev-dashboard.s3.amazonaws.com")
+@CrossOrigin(origins = "http://dhobighat-dev.s3.amazonaws.com")
 public class WelcomeController {
 
     @GetMapping("/getMessage")
